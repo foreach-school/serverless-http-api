a2547b68d8109f82c62e4b7f5ddeaf5eae7cfc5f
foreach-school <162062403+foreach-school@users.noreply.github.com>
2024-04-09 21:57:45 -0300

    Update index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -5,9 +5,9 @@ import { UserDto } from '../dto/UserDto';
 
 export const handler = async (event: APIGatewayEvent) => {
   try {
-    const dto: UserDto = await UserService.get(event.pathParameters['username']);
+    //const dto: UserDto = await UserService.get(event.pathParameters['username']);
 
-    return httpResponse(200, dto);
+    return httpResponse(200, process.env.TST);
   } catch (error) {
     return httpResponse(error.statusCode || 500, error.message || 'Internal server error');
   }
